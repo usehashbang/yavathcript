8078723945236a68e433d9268aff4b0b6b86c61a
Dan Sparks <dan.sparks@clever.com>
2014-06-29 13:19:23 -0700

    Update bash scripts
    Fixed error that test.sh didn't delete the js files when it is done.
    Renamed compile.sh to build.sh.

--- test.sh
+++ test.sh
@@ -2,4 +2,5 @@
 
 coffee -c -o test/ test/yth_test.coffee test/helpers.coffee
 node_modules/mocha/bin/mocha test/yth_test.js
-rm test/yth_test.js, test/helpers.js
+rm test/yth_test.js
+rm test/helpers.js
