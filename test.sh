a2930511139a92f7679f7a61dd6a1e906ed8aaff
Dan Sparks <dan.sparks@clever.com>
2014-06-29 13:17:02 -0700

    Add tests for logical operators
    Also, created helpers module for tests.

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-coffee -c -o test/ test/yth_test.coffee
+coffee -c -o test/ test/yth_test.coffee test/helpers.coffee
 node_modules/mocha/bin/mocha test/yth_test.js
-rm test/yth_test.js
+rm test/yth_test.js, test/helpers.js
